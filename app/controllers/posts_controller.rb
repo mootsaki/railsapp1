dc34719322134f278e890a65df27d6ba87f2df5f
mootsaki <mootstylin@gmail.com>
2018-08-18 01:03:41 -0600

    Adding a posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+
+    # Crud
+
+#view
+    def index
+    end
+# view
+    def show
+    end
+
+# view
+    def new
+    end
+
+    def create 
+    end
+
+    def edit
+    end
+# view
+    def update
+    end
+
+    def delete
+    end
+
+end
